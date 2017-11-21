8f0dbc17fdf9445386801ba88cb74213097fac0a
han8909227 <hbao2016@hotmail.com>
2017-11-20 21:39:02 -0800

    added some codz

--- django_imager/imager_profile/models.py
+++ django_imager/imager_profile/models.py
@@ -1,3 +1,4 @@
+"""Model class."""
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -5,12 +6,21 @@ from django.contrib.auth.models import User
 # Create your models here.
 class ImagerProfile(models.Model):
     """Profile for the user of the application."""
-    website = models.CharField(max_length=200, blank=True, null=True)
-    location = models.CharField(max_length=200, blank=True, null=True)
-    fee = models.FloatField(max_length=200, blank=True, null=True)
-    camera = models.CharField(max_length=200, blank=True, null=True)
-    services = models.CharField(max_length=200, blank=True, null=True)
-    bio = models.CharField(max_length=200, blank=True, null=True)
-    phone = models.CharField(max_length=200, blank=True, null=True)
-    photo_styles = models.CharField(max_length=200, blank=True, null=True)
-    user = models.OneToOneField(User, related_name='profile')
+    is_active = models.BooleanField(default=False)
+    if is_active:
+        website = models.URLField(max_length=200, blank=True, null=True)
+        location = models.CharField(max_length=200, blank=True, null=True)
+        fee = models.FloatField(max_length=200, blank=True, null=True)
+        camera = models.CharField(max_length=200, blank=True, null=True)
+        services = models.CharField(max_length=200, blank=True, null=True)
+        bio = models.CharField(max_length=200, blank=True, null=True)
+        phone = models.CharField(max_length=200, blank=True, null=True)
+        photo_styles = models.CharField(max_length=200, blank=True, null=True)
+        user = models.OneToOneField(User, related_name='profile')
+    else:
+        raise Exception('no access privilege')
+
+    def active(self):
+        """Give permission for active user, loggin in."""
+        self.is_active = True
+        return
