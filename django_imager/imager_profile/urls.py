d3235f438f3b6cbfc9f2716d0339d4da2cefaac6
han8909227 <hbao2016@hotmail.com>
2017-11-23 15:02:23 -0800

    added travis

--- django_imager/imager_profile/urls.py
+++ django_imager/imager_profile/urls.py
@@ -1,8 +1,8 @@
 """URL for the application imager_profile."""
-from django.conf.urls import url
-
-import views
+from django.conf.urls import url, include
+from django.contrib.auth.views import login, logout
 
 urlpatterns = [
-,
+    url(r'^registration/', include("registration.backends.simple.urls")),
+
 ]
