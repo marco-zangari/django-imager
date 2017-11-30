293125bbc8f4f2d69e8d8c088713236fc1800233
han8909227 <hbao2016@hotmail.com>
2017-11-30 14:07:46 -0800

    coverted all existing view funs to class based funs

--- django_imager/imager_images/urls.py
+++ django_imager/imager_images/urls.py
@@ -1,12 +1,11 @@
 from django.conf.urls import url
-from imager_images.views import library_view, album_view, albums_view, photos_view
-from imager_images.views import PhotoDetailView
+from imager_images.views import PhotoDetailView, AlbumsView, PhotosView, AlbumView, LibraryView
 
 urlpatterns = [
-    url(r'^$', library_view, name='library'),
-    url(r'library/$', library_view, name='library'),
-    url(r'albums$', albums_view, name='albums'),
-    url(r'photos$', photos_view, name='photos'),
-    url(r'album/(?P<album_id>\d+)/$', album_view, name='album'),
+    url(r'^$', LibraryView.as_view(), name='library'),
+    url(r'library/$', LibraryView.as_view(), name='library'),
+    url(r'albums$', AlbumsView.as_view(), name='albums'),
+    url(r'photos$', PhotosView.as_view(), name='photos'),
+    url(r'album/(?P<album_id>\d+)/$', AlbumView.as_view(), name='album'),
     url(r'photo/(?P<pk>\d+)/$', PhotoDetailView.as_view(), name='photo'),
 ]
--- django_imager/imager_images/views.py
+++ django_imager/imager_images/views.py
@@ -4,26 +4,41 @@
 from django.shortcuts import render
 from imager_profile.models import ImagerProfile
 from django.views.generic.detail import DetailView
+from django.views.generic import ListView
 from imager_images.models import Album, Photo
 
 # Create your views here.
 
 
-def library_view(request):
-    """Library view handling function."""
-    profile = ImagerProfile.active.filter(user__username=request.user.username).first()
-    photos = profile.photos.all()
-    albums = profile.albums.all()
-    username = request.user.username
+class LibraryView(ListView):
+    """Handle library view request."""
 
-    return render(request, 'imager_images/library.html', {'photos': photos, 'profile': profile, 'albums': albums, 'username': username})
+    template_name = 'imager_images/library.html'
+
+    def get_context_data(self):
+        """Return proper context."""
+        profile = ImagerProfile.active.filter(user__username=self.request.user.username).first()
+        photos = profile.photos.all()
+        albums = profile.albums.all()
+        username = self.request.user.username
+        return {'photos': photos, 'profile': profile, 'albums': albums, 'username': username}
+
+    def get_queryset(self):
+        """Override get_query."""
+        return {}
 
 
-def album_view(request, album_id):
-    """Album view handling function."""
-    album = Album.objects.filter(id=album_id).first()
-    photos = album.photos.all()
-    return render(request, 'imager_images/album.html', {'album': album, 'photos': photos})
+class AlbumView(ListView):
+    """Handle single album view request."""
+
+    template_name = 'imager_images/album.html'
+
+    def get_context_data(self):
+        """Return proper context."""
+        album = Album.objects.get(id=self.kwargs['album_id'])
+        if album.published == 'PRIVATE':
+            photos = album.photos.all()
+            return {'album': album, 'photos': photos}
 
 
 class PhotoDetailView(DetailView):
@@ -33,13 +48,31 @@ class PhotoDetailView(DetailView):
     model = Photo
 
 
-def albums_view(request):
-    """Return all album, cover photo. Albums view handling function."""
-    albums = Album.objects.all()
-    return render(request, 'imager_images/albums.html', {'albums': albums})
+class AlbumsView(ListView):
+    """Handle all album view request."""
+
+    template_name = 'imager_images/albums.html'
+
+    def get_context_data(self):
+        """Return all album instances."""
+        albums = Album.objects.all()
+        return {'albums': albums}
+
+    def get_queryset(self):
+        """Override get_query."""
+        return {}
 
 
-def photos_view(request):
-    """Return all photos, cover photo. photos view handling function."""
-    photos = Photo.objects.all()
-    return render(request, 'imager_images/photos.html', {'photos': photos})
+class PhotosView(ListView):
+    """Handle all photos view request."""
+
+    template_name = 'imager_images/photos.html'
+
+    def get_context_data(self):
+        """Return all album instances."""
+        photos = Photo.objects.all()
+        return {'photos': photos}
+
+    def get_queryset(self):
+        """Override get_query."""
+        return {}
